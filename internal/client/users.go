ef6d5bd162976547bcccd96ab8a5865cd5a54ad6
Fabio Volpe <volpefabio@gmail.com>
2023-07-30 19:01:37 +0100

    not needed fields removed

--- internal/client/users.go
+++ internal/client/users.go
@@ -5,17 +5,14 @@ import (
 )
 
 type User struct {
-	Url                string    `json:"url"`
-	FirstName          string    `json:"first_name"`
-	LastName           string    `json:"last_name"`
-	Email              string    `json:"email"`
-	Role               string    `json:"role"`
-	PermissionLevel    int       `json:"permission_level"`
-	NiNumber           string    `json:"ni_number"`
-	UniqueTaxReference string    `json:"unique_tax_reference"`
-	OpeningMileage     string    `json:"opening_mileage"`
-	UpdatedAt          time.Time `json:"updated_at"`
-	CreatedAt          time.Time `json:"created_at"`
+	Url             string    `json:"url"`
+	FirstName       string    `json:"first_name"`
+	LastName        string    `json:"last_name"`
+	Email           string    `json:"email"`
+	Role            string    `json:"role"`
+	PermissionLevel int       `json:"permission_level"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	CreatedAt       time.Time `json:"created_at"`
 }
 
 func PersonalProfile() (*User, error) {
